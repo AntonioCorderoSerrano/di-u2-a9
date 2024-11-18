ba8cfcc18a4cfdbb7f97fe22e7da9cdb56944f93
Antonio Cordero Serrano <antoniocorderoserrano@gmail.com>
2024-11-18 11:51:46 +0100

    Finalizado

--- src/app/processQueue.js
+++ src/app/processQueue.js
@@ -1,8 +1,14 @@
 export function getFinalState(baseState, queue) {
     let finalState = baseState;
-  
-    // TODO: haz algo con la cola...
-  
+    
+    for(let i = 0; i < queue.lenght; i++){
+      if(typeof queue[i] === 'function'){
+        finalState = queue[i](finalState);
+      }else{
+        finalState = queue[i];
+      }
+    }
+    
     return finalState;
   }
   
